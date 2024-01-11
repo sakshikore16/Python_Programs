5e4a5ee1ad95888900047b7f20a62903c55ee217
Sakshi Kore <142594806+sakshikore16@users.noreply.github.com>
2024-01-11 11:15:50 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,16 @@
+# Star Pattern
+
+n = int(input("Enter Number Of Rows: "))
+col = n+n-5
+mid = col//2
+
+for i in range(n):
+    for j in range(col):
+        
+        if i == 2 or i == (n - 3) or i + j == mid or j - i == mid or i - j == 2 or i + j == col + 1:
+            print("*", end=" ")
+        
+        else:
+            print(" ", end=" ")
+    
+    print()
