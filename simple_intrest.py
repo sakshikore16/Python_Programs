3e8154eeb146f5a8d6415eb57dc879b30efe8d13
Sakshi Kore <142594806+sakshikore16@users.noreply.github.com>
2024-01-11 11:13:25 +0530

    Add files via upload

---
+++ simple_intrest.py
@@ -0,0 +1,8 @@
+p = int(input("Enter Principle Amount: ₹"))
+r = int(input("Enter Rate Of Intrest: "))
+t = int(input("Enter Time Period: "))
+
+si = p*r*t/100
+
+print ("Simple Intrest: ", si)
+
---
+++ student_marks.py
@@ -0,0 +1,15 @@
+# WAP to get the class or student wrt marks entered
+
+x = int(input ('Enter The marks: '))
+
+if (x >= 70):
+    print ('Distinction')
+
+elif (x >= 60):
+    print ('First class')
+
+elif (x >= 50):
+    print ('Second class')
+
+else:
+    print ('Pass class')
